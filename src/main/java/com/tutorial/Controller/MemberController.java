3fc574d65e546ecc3c7d853ed40f571b8c709858
Jason Chiu 邱顯鈞 <jasonchiu@hitrust.com.tw>
2021-04-27 22:13:54 +0800

    D13

--- src/main/java/com/tutorial/Controller/MemberController.java
+++ src/main/java/com/tutorial/Controller/MemberController.java
@@ -6,7 +6,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tutorial.Dao.MemberRepository;
 import com.tutorial.Model.MemberAccount;
@@ -15,39 +18,51 @@ import com.tutorial.Service.MemberService;
 
 @Controller
 public class MemberController {
-	
+
 	@Autowired
 	MemberAccount memberAccount;
-	
+
 	@Autowired
 	MemberRepository memberRepository;
-	
+
 	@Autowired
 	MemberService memberService;
-	
+
+	@GetMapping("/login")
+	public String login(@ModelAttribute MemberAccountJPA memberAccountJPA) {
+		return "login";
+	}
+
+	@PostMapping("/doLogin")
+	public String doLogin(@ModelAttribute MemberAccountJPA memberAccountJPA) {
+		System.out.println(
+				memberRepository.findCheckMemberAccount(memberAccountJPA.getEmail(), memberAccountJPA.getPassword()));
+		return "welcome";
+	}
+
 	@GetMapping("/addMemberPage")
 	public String addMemberPage() {
 		List<MemberAccountJPA> memberAccountJPA = new ArrayList<MemberAccountJPA>();
 		memberAccountJPA = memberRepository.findAll();
-		
+
 		for (int i = 0; i < memberAccountJPA.size(); i++) {
 			System.out.println(memberAccountJPA.get(i).getId());
 		}
-		
+
 		return "addMemberPage";
 	}
-	
+
 	@RequestMapping("/")
+	@ResponseBody
 	public String hello() {
 		memberAccount = new MemberAccount();
-		memberAccount.setPassword("12345678");
+		memberAccount.setPassword("1234");
 		memberAccount.setEmail("test@gmail.com");
 		memberAccount.setCellphone("0912345678");
 		memberAccount.setAddress("台北市");
-		
+
 		memberService.addMember(memberAccount);
 		return "新增會員成功";
 	}
-	
-	
+
 }
--- src/main/java/com/tutorial/Dao/MemberRepository.java
+++ src/main/java/com/tutorial/Dao/MemberRepository.java
@@ -1,9 +1,14 @@
 package com.tutorial.Dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.tutorial.Model.MemberAccountJPA;
 
-public interface MemberRepository extends JpaRepository<MemberAccountJPA, Long>{
+public interface MemberRepository extends JpaRepository<MemberAccountJPA, Long> {
 
+	@Query(value = "select id,email,password,address,cellphone from spring30mvc.member_account where EMAIL = ?1 and PASSWORD = ?2 ", nativeQuery = true)
+	List<MemberAccountJPA> findCheckMemberAccount(String email, String password);
 }
